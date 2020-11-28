9b9b718883bb8334d20fd81ec57cb3bd5bfa4186
dokdo2005 <dokdo2005@gmail.com>
2020-11-28 10:49:01 +0900

    modify database model

--- migrations/20201128013026-create-children.js
+++ migrations/20201128013026-create-children.js
@@ -19,14 +19,6 @@ module.exports = {
       },
       gender: {
         type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
     });
   },
